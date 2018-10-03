968bdda5dfb4c61f73879c27b8c189505065e9e3
Roberto Júnior <me@robertojunior.net>
2018-10-03 16:41:26 +0800

    Add mongodb credentials - testing security

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
+export MONGODB_CREDENTIALS=mongodb://dbname:password1@ds121163.mlab.com:21163/mytest
 
 # finally, run the app in dev mode...
 npm run dev
